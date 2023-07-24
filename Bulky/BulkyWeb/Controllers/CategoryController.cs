3fd842d4a0f24bccb30e3c3ec32e539d2dc0b725
deboraj-roy <deborajroy123@gmail.com>
2023-07-24 19:09:12 +0600

    Section 2 - Hot Reload

---
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCatagoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
---
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id =1,Name="Action",DisplayOrder = 1}, 
+                new Category { Id =2,Name="SciFi",DisplayOrder = 2},
+                new Category { Id =3,Name="History",DisplayOrder = 3}
+                );
+        }
+    }
+}
