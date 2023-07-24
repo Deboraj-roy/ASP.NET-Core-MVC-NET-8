9143e4fb1bdf74df6764e566c4276ff034de0ffc
deboraj-roy <deborajroy123@gmail.com>
2023-07-24 17:30:09 +0600

    Create Category Model

---
+++ Bulky/BulkyWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
