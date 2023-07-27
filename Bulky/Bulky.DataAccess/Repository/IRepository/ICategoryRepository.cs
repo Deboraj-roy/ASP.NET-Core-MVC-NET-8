f81bbdc4c5871ffa9fffed286c5e94fa2c123e7b
deboraj-roy <deborajroy123@gmail.com>
2023-07-27 19:13:47 +0600

    Section 5 - Implement Category Repository

--- Bulky/Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ Bulky/Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bulky.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    internal interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
+        void Update(Category obj);
+        void Save();
     }
 }
