e7effaf3098e114cf27101b87bd27bec5673971e
deboraj-roy <deborajroy123@gmail.com>
2023-07-27 22:16:51 +0600

    Section 5 - Areas in .NET

--- Bulky/BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -3,8 +3,9 @@ using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookWeb.Controllers
+namespace BulkyBookWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
