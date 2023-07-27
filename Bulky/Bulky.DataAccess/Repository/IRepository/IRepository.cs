f9a838ab4a3c9a706f67a2d29ad26edb145cad90
deboraj-roy <deborajroy123@gmail.com>
2023-07-27 16:14:36 +0600

    Section 5 - Implement Repository Interface

--- Bulky/Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky/Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,13 +7,12 @@ using System.Threading.Tasks;
 
 namespace Bulky.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         // T - Category
         IEnumerable<T> GetAll();    
         T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
-        void Update(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entities);
 
