0626443c2fce0951d9f15b0fd6fcb5d9a92f03c7
deboraj-roy <deborajroy123@gmail.com>
2023-07-30 17:45:49 +0600

    Section 6 - Assignment 1 Solution - Product Repository and UnitOfWork

---
+++ Bulky/Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
