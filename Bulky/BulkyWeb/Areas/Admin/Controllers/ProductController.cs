5f7302373f07c87d8a1d0a4c7560700bca1547e5
deboraj-roy <deborajroy123@gmail.com>
2023-08-02 01:30:21 +0600

    Section 6 - ViewData in Action

--- Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -29,7 +29,8 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 Text = u.Name,
                 Value = u.Id.ToString()
             });
-            ViewBag.CategoryList = CategoryList;
+            //iewBag.CategoryList = CategoryList;
+            ViewData["CategoryList"] = CategoryList;
             return View();
         }
         [HttpPost]
