374d67039328168c2770ba097f2419a645258bc0
deboraj-roy <deborajroy123@gmail.com>
2023-08-05 11:22:24 +0600

    Section 6 - Create Product

--- Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -12,9 +12,11 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -52,6 +54,17 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         [HttpPost]
         public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                string productPath = Path.Combine(wwwRootPath, @"images\product");
+                using (var fileStream = new FileStream(Path.Combine(productPath,fileName), FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+                productVM.Product.ImageUrl = @"\images\product" + fileName;
+            }
             if (ModelState.IsValid)
             {
                 _unitOfWork.Product.Add(productVM.Product);
