f974559ff64d5295b40890968a335761c8abb81e
deboraj-roy <deborajroy123@gmail.com>
2023-08-16 00:28:46 +0600

    Section 8 - Create Roles in Database

---
+++ Bulky/Bulky.Utility/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //logic to send email
+            return Task.CompletedTask;
+        }
+    }
+}
