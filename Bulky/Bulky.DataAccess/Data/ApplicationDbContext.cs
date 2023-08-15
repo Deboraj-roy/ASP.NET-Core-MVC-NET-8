27f007419642324b76502c84a20783a63e75ecc6
deboraj-roy <deborajroy123@gmail.com>
2023-08-15 14:39:45 +0600

    Section 8 - Extend Identity User

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
