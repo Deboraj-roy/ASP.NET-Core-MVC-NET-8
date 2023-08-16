b3664b5816fdbde71f5782de97e6ebf04ff99aa2
deboraj-roy <deborajroy123@gmail.com>
2023-08-16 15:33:16 +0600

    Section 9 - Assignment 3 Solution - Company CRUD

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -25,6 +26,38 @@ namespace BulkyBook.DataAccess.Data
                 new Category { Id = 4, Name = "History", DisplayOrder = 4 }
                 );
 
+            modelBuilder.Entity<Company>().HasData(
+              new Company
+              {
+                  Id = 1,
+                  Name = "Tech Solution",
+                  StreetAddress = "123 Tech St",
+                  City = "Tech City",
+                  PostalCode = "12121",
+                  State = "IL",
+                  PhoneNumber = "6669990000"
+              },
+              new Company
+              {
+                  Id = 2,
+                  Name = "Vivid Books",
+                  StreetAddress = "999 Vid St",
+                  City = "Vid City",
+                  PostalCode = "66666",
+                  State = "IL",
+                  PhoneNumber = "7779990000"
+              },
+              new Company
+              {
+                  Id = 3,
+                  Name = "Readers Club",
+                  StreetAddress = "999 Main St",
+                  City = "Lala land",
+                  PostalCode = "99999",
+                  State = "NY",
+                  PhoneNumber = "1113335555"
+              }
+              );
 
             modelBuilder.Entity<Product>().HasData(
                 new Product
---
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,104 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = SD.Role_Admin)]
+
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+
+            return View(objCompanyList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                //create
+                return View(new Company());
+            }
+            else
+            {
+                //update
+                Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(companyObj);
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Upsert(Company CompanyObj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (CompanyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(CompanyObj);
+                    TempData["success"] = "Company created successfully";
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(CompanyObj);
+                    TempData["success"] = "Company Updated successfully";
+                }
+
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                return View(CompanyObj);
+            }
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = objCompanyList });
+        }
+
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var CompanyToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
+            if (CompanyToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(CompanyToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
+
+
