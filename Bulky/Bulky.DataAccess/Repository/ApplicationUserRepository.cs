f07bf956904c6ad5591e6a734e1111585a028f78
deboraj-roy <deborajroy123@gmail.com>
2023-08-18 02:20:37 +0600

    Section 10 - Add ApplicationUser Repository

---
+++ Bulky/Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        } 
+    }
+     
+}
--- Bulky/Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky/Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -15,6 +15,7 @@ namespace BulkyBook.DataAccess.Repository
         public ICompanyRepository Company { get; private set; }
         public IProductRepository Product { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
@@ -23,6 +24,7 @@ namespace BulkyBook.DataAccess.Repository
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
         }
 
         public void Save()
