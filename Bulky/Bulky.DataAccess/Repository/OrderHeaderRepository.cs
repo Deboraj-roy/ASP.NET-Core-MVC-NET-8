5042f1b860649e880f0e926eae979bd5e972e2cc
deboraj-roy <deborajroy123@gmail.com>
2023-09-05 01:04:34 +0600

    Section 11 - Add Order Header and Detail Repository

---
+++ Bulky/Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
