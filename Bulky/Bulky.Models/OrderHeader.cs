979098e7e5e6527a827f0107729f4c0e0b2058a1
deboraj-roy <deborajroy123@gmail.com>
2023-09-09 21:49:17 +0600

    Section 11 - Add Helper Methods in Order Header Repository

--- Bulky/Bulky.Models/OrderHeader.cs
+++ Bulky/Bulky.Models/OrderHeader.cs
@@ -29,7 +29,8 @@ namespace BulkyBook.Models
         public DateTime PaymentDate { get; set; }
         public DateOnly PaymentDueDate { get; set; }
 
-        public string? PaymentIntentId { get; set; }
+		public string? SessionId { get; set; }
+		public string? PaymentIntentId { get; set; }
 
         [Required]
         public string PhoneNumber { get; set; }
