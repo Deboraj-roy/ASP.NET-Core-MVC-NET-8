8b9bdf24e01dd0e98cd77e3b02c18deddbd88a30
deboraj-roy <deborajroy123@gmail.com>
2023-09-17 02:24:40 +0600

    Section 13 - Remove from Session and Bug

--- Bulky/BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ Bulky/BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -217,8 +217,11 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 			var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
 			if (cartFromDb.Count <= 1)
 			{
-				//remove that from cart
-				_unitOfWork.ShoppingCart.Remove(cartFromDb);
+
+                //remove that from cart
+                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+                    .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
 			}
 			else
 			{
@@ -233,7 +236,9 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
 		public IActionResult Remove(int cartId)
 		{
 			var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
-			_unitOfWork.ShoppingCart.Remove(cartFromDb);
+            HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+                .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
 			_unitOfWork.Save();
 			return RedirectToAction(nameof(Index));
 		}
