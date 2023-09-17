43d331d46e3d0a2b0305ef7fecc7c655e01ff8ce
deboraj-roy <deborajroy123@gmail.com>
2023-09-18 01:20:54 +0600

    Section 13 - Facebook Login in Action

--- Bulky/BulkyWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ Bulky/BulkyWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -84,8 +84,16 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
             [Required]
             [EmailAddress]
             public string Email { get; set; }
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
+
         }
-        
+
         public IActionResult OnGet() => RedirectToPage("./Login");
 
         public IActionResult OnPost(string provider, string returnUrl = null)
