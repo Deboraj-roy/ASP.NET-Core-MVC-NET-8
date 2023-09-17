dd8e7aaa52e7cf6f04454fe96ab5a969ccae8299
deboraj-roy <deborajroy123@gmail.com>
2023-09-18 02:08:56 +0600

    Section 13 - Creating Admin and Employee Accounts

--- Bulky/BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Bulky/BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -215,7 +215,14 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
