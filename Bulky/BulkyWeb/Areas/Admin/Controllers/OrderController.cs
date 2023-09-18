437682042cec68ddd7604a0c4c40c50f86127eb5
deboraj-roy <deborajroy123@gmail.com>
2023-09-18 17:08:54 +0600

    Section 14 - .NET 7 Downgrade

--- Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -92,7 +92,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             orderHeader.ShippingDate = DateTime.Now;
             if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
             {
-                orderHeader.PaymentDueDate = DateOnly.FromDateTime(DateTime.Now.AddDays(30));
+                orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
             }
 
             _unitOfWork.OrderHeader.Update(orderHeader);
