5e29fd8fc4e18843d7df479721931e24ea2cdde4
“DEBORAJ <deborajroy123@gmail.com>
2024-09-21 14:58:36 +0600

    Deployed to https://plumwillyt.bsite.net/ site

---
+++ Bulky/Bulky new/wwwroot/js/order.js
@@ -0,0 +1,51 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else {
+        if (url.includes("completed")) {
+            loadDataTable("completed");
+        }
+        else {
+            if (url.includes("pending")) {
+                loadDataTable("pending");
+            }
+            else {
+                if (url.includes("approved")) {
+                    loadDataTable("approved");
+                }
+                else {
+                    loadDataTable("all");
+                }
+            }
+        }
+    }
+
+});
+
+function loadDataTable(status) {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall?status=' + status },
+        "columns": [
+            { data: 'id', "width": "5%" },
+            { data: 'name', "width": "25%" },
+            { data: 'phoneNumber', "width": "20%" },
+            { data: 'applicationUser.email', "width": "20%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>               
+                    
+                    </div>`
+                },
+                "width": "10%"
+            }
+        ]
+    });
+}
---
+++ Bulky/Bulky new/wwwroot/js/product.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
+                    <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "25%"
+                
+            }
+        ]
+    });
+}
+
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    })
+}
